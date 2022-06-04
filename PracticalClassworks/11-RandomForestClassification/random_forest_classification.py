9aef15759c631435cd1e6411db4d77b6688fbe33
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-06-05 02:38:36 +0300

    Update random_forest_classification.py

--- PracticalClassworks/11-RandomForestClassification/random_forest_classification.py
+++ PracticalClassworks/11-RandomForestClassification/random_forest_classification.py
@@ -1,6 +1,5 @@
 # Gerekli kütüphaneleri import edelim
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
